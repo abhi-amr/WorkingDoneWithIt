5c60f455819476916a552380e3040a02d540bced
Abhishek Kumar <40247925+abhi-amr@users.noreply.github.com>
2022-01-28 23:47:54 +0530

    image not cached but ok

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,24 @@
 import { create } from 'apisauce';
+import cache from '../utility/cache';
 
 const apiClient = create({
     baseURL : 'https://jsonplaceholder.typicode.com/'
 });
+
+//for caching ----images not cached... fast-images is now in expo-sdk
+const get = apiClient.get;
+apiClient.get = async (url, params, axiosConfig) => {
+
+    const response = await get(url, params, axiosConfig);
+
+    if(response.ok){
+        cache.store(url, response.data);
+        return response;
+    }
+
+    const data = await cache.get(url);
+    return data ? {ok : true, data} : response;
+}
+//------
 
 export default apiClient;
